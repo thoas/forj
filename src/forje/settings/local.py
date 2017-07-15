96e24faa575aea424f6af8fa7f65b4e87cb35d5a
Florent Messa <florent.messa@gmail.com>
2017-07-15 17:16:51 +0200

    Add debug toolbar in INSTALLED_APPS

--- src/forje/settings/local.py
+++ src/forje/settings/local.py
@@ -1,1 +1,13 @@
 from .base import *  # noqa
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
