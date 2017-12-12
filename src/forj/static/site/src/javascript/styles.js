21b81fc3269f1b9b9fd0c53178139aba5fd8187d
Florent Messa <florent.messa@gmail.com>
2017-12-12 23:03:56 +0100

    Add table.bin

--- src/forj/static/site/src/javascript/styles.js
+++ src/forj/static/site/src/javascript/styles.js
@@ -1,2 +1,2 @@
-require.context('../assets', true, /^\.\/.*\.(png|jpg|gif|svg|eot|svg|ttf|woff|woff2|otf)$/)
+require.context('../assets', true, /^\.\/.*\.(png|jpg|gif|svg|eot|svg|ttf|woff|woff2|otf|bin)$/)
 require.context('../shaders', true, /^\.\/.*\.(frag)$/)
